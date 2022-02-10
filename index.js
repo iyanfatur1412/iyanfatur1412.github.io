bf2d7d79829dc8d86ddf2f47361cd1927a8ce4c9
iyanfatur <iyanfatur1412@gmail.com>
2022-02-10 09:31:28 +0700

    -ver 1.0.1

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-// let viewResponsive = 2;
+let viewResponsive = 2;
 
-// if(document.body.offsetWidth >= 1024) {
-//     viewResponsive = 1;
-// }else if(document.body.offsetWidth >= 640) {
-//     viewResponsive = 3;
-// }
+if(document.body.offsetWidth >= 1024) {
+    viewResponsive = 1;
+}else if(document.body.offsetWidth >= 640) {
+    viewResponsive = 3;
+}
 
-// const swiper = new Swiper('.swiper', {
-//     loop: true,
-//     slidesPerView: viewResponsive,
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//     autoplay: false
-// });
-// const sw = document.querySelector(".swiper").swiper;
-// sw.slideNext();
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: viewResponsive,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: false
+});
+const sw = document.querySelector(".swiper").swiper;
+sw.slideNext();
 
 const profil = document.querySelector(".profil");
 const about = document.querySelector(".about");
