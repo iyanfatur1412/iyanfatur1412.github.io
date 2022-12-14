361e878eacdb15f00649d10ee5bb152a7040d40b
Septian Paturahman <86770512+iyanfatur1412@users.noreply.github.com>
2022-12-15 01:25:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let viewResponsive = 2;
+
+if(document.body.offsetWidth >= 1024) {
+    viewResponsive = 1;
+}else if(document.body.offsetWidth >= 640) {
+    viewResponsive = 3;
+}
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: viewResponsive,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: false
+});
+const sw = document.querySelector(".swiper").swiper;
+sw.slideNext();
+
+const profil = document.querySelector(".profil");
+const about = document.querySelector(".about");
+const contact = document.querySelector(".contact");
+let slideBottom = "slideBottom .9s ease-in forwards";
+
+if(window.innerWidth < 1024) {
+    window.addEventListener("scroll", function() {
+        if(this.scrollY >= (profil.scrollHeight - 100)) {
+            about.style.animation = slideBottom;
+        }
+        if(this.scrollY >= (profil.scrollHeight + about.scrollHeight + 50)) {
+            contact.style.animation = slideBottom;
+        }
+    })
+}
